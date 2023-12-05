11f2ba3d5a41dc31f2d574461c8b9455658c25b0
cyrusnaficy <cyrus.naficy@gmail.com>
2023-12-05 08:50:34 -0500

    motor mirror

--- subsystem/drivetrain.py
+++ subsystem/drivetrain.py
@@ -1,31 +1,43 @@
 import config
 
-from ctre import TalonFX, ControlMode
+from ctre import TalonFX, ControlMode, FollowerType
 from subsystem.config.subsystem_base import Subsystem
 
 class Drivetrain(Subsystem):
     
     def init(self) -> None:
-        self.left_motors = [TalonFX(config.CAN_IDS_DRIVETRAIN['left_1']), TalonFX(config.CAN_IDS_DRIVETRAIN['left_2'])]
-        self.right_motors = [TalonFX(config.CAN_IDS_DRIVETRAIN['right_1']), TalonFX(config.CAN_IDS_DRIVETRAIN['right_2'])]
+        self.left_motor = TalonFX(config.CAN_IDS_DRIVETRAIN['left_1'])
+        self.left_motor_mirror =  TalonFX(config.CAN_IDS_DRIVETRAIN['left_2'])
+        self.left_motor_mirror.follow(self.left_motor, FollowerType.PercentOutput)
+
+        self.right_motor = TalonFX(config.CAN_IDS_DRIVETRAIN['right_1'])
+        self.right_motor_mirror = TalonFX(config.CAN_IDS_DRIVETRAIN['right_2'])
+        self.right_motor_mirror.follow(self.right_motor, FollowerType.PercentOutput)
 
     def set_raw_output(self, speed: float, is_left: bool) -> None: 
-        for motor in (self.left_motors if is_left else self.right_motors):
-            motor.set(ControlMode.PercentOutput, speed)
+        if is_left:
+            self.left_motor.set(ControlMode.PercentOutput, speed)
+        else:
+            self.right_motor.set(ControlMode.PercentOutput, speed)
 
     def set_velocity(self, velocity: float, is_left: bool) -> None:
-        for motor in (self.left_motors if is_left else self.right_motors):
-            motor.set(ControlMode.Velocity, velocity)
+        if is_left:
+            self.left_motor.set(ControlMode.Velocity, velocity)
+        else:
+            self.right_motor.set(ControlMode.Velocity, velocity)
 
     def get_raw_output(self, is_left: bool) -> float:
-        for motor in (self.left_motors if is_left else self.right_motors):
-            return motor.getMotorOutputPercent()
+        if is_left:
+            return self.left_motor.getMotorOutputPercent()
+        else:
+            return self.right_motor.getMotorOutputPercent()
 
-        
     def get_velocity(self, is_left: bool) -> float:
-        for motor in (self.left_motors if is_left else self.right_motors):
-            return motor.getSelectedSensorVelocity()
+        if is_left:
+            return self.left_motor.getSelectedSensorVelocity()
+        else:
+            return self.right_motor.getSelectedSensorVelocity()
     
     def stop(self) -> None:
-        for motor in self.left_motors + self.right_motors:
-            motor.set(ControlMode.PercentOutput, 0)
+        self.left_motor.set(ControlMode.PercentOutput, 0)
+        self.right_motor.set(ControlMode.PercentOutput, 0)
