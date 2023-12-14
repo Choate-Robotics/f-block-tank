d994f4fb62bd3bd5f573b80e574eab22e3feafba
cyrusnaficy <cyrus.naficy@gmail.com>
2023-12-14 09:57:30 -0500

    final can

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 CAN_IDS_MANIPULATOR = {
-    'left_motor': 1,
-    'right_motor': 2,
+    'left_motor': 5,
+    'right_motor': 6,
 }
 CAN_IDS_DRIVETRAIN = {
-    'left_1': 4,
-    'left_2': 5,
-    'right_1': 6,
-    'right_2': 7
+    'left_1': 1,
+    'left_2': 2,
+    'right_1': 3,
+    'right_2': 4
 }
